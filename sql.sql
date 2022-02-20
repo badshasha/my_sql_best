5e1e2ee44f162fced81dd95ac4776883e921c1fc
badshasha <shavendragoesoft@gmail.com>
2022-02-20 05:31:22 -0800

    create table

--- sql.sql
+++ sql.sql
@@ -1,1 +1,19 @@
-show databases;
+create table table_name(
+
+    id int primary key AUTO_INCREMENT not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email_address varcahr(20) not null,
+
+
+)
+
+create table  if not exists table_name(
+
+    id int primary key AUTO_INCREMENT not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email_address varcahr(20) not null,
+
+
+)
