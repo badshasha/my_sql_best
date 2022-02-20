c988d8cf063194e9a427cf12330ca59e25fbb747
badshasha <shavendragoesoft@gmail.com>
2022-02-20 06:56:32 -0800

    select statement

--- sql.sql
+++ sql.sql
@@ -1,22 +1,3 @@
+select * from table_name;
 
-create table ClassRoom(
-    id int AUTO_INCREMENT not null,      
-
-)
-
-create table Student(
-
-    id int primary key AUTO_INCREMENT,
-    f_name VARCHAR(255) not null,
-    
-    -- foregin key 
-    class_id int,
-
-    -- references
-    foreign key(class_id) REFERENCES ClassRoom(id) on delete CASCADE,
-    
-)
-
-alter table Student
-     foreign key(class_id) REFERENCES ClassRoom(id) on delete CASCADE,
-
+select * from database_name.table_name;
