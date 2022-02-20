feab7acf4d65181d5be8d776b93de1fb79ff8c54
badshasha <shavendragoesoft@gmail.com>
2022-02-20 07:51:47 -0800

    limit keyword

--- sql.sql
+++ sql.sql
@@ -1,19 +1,10 @@
----------------------- having ---------------
+-------------------------------------------
+------------- limit -----------------------
+-------------------------------------------
 
 select 
-    first_name , 
-    count(first_name) as name_count ,
-    hire_date
-from    
-    employees 
-where 
-    hire_date >= '1999-01-01'
-group by 
-    first_name 
-having count(first_name) < 200
-order by first_name ASC
-
-working with aggragartion function you should need to user HAVING 
-
-
-you cannot have both non-aggragated and aggregated condition in a same class 
+    * 
+from 
+    salary 
+order by salary DESC 
+limit 10;
