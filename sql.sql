9ded659861e5570777808278ae34a1ec9050be8a
badshasha <shavendragoesoft@gmail.com>
2022-02-20 07:03:44 -0800

    where with and keyword

--- sql.sql
+++ sql.sql
@@ -1,15 +1,18 @@
--- select column form tables
+-- advance select with where keyword 
 
-select colum1 , colum2 
-    from table_name 
-
--- select all element from table 
-
-select * form table_name;
-
--- user where keywrok 
 
 select first_name 
     from emp_table 
     where emp_table.gender = 'M';
 
+
+-- and key word 
+-- select employees M and first name Denis 
+
+SELECT 
+    first_name, last_name, gender
+FROM
+    employees
+WHERE
+    employees.gender = 'M'
+        AND employees.first_name = 'Denis';
