d1ec2f3597bb2fa3d895a9c58946100324d54b3e
badshasha <shavendragoesoft@gmail.com>
2022-02-20 06:59:00 -0800

    advance primary key different way

--- sql.sql
+++ sql.sql
@@ -1,1 +1,9 @@
-drop table table_name;
+
+create table sales(
+	purchase_number Int auto_increment,
+        date_of_purchase date,
+        customer_id Int,
+        item_code varchar(10),
+    
+    primary key ( purchase_number)
+);
