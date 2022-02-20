aed26edf376e06d6f8996607aafbd93add85df24
badshasha <shavendragoesoft@gmail.com>
2022-02-20 07:47:16 -0800

    having

--- sql.sql
+++ sql.sql
@@ -1,15 +1,19 @@
----------------------- group by command ---------------
+---------------------- having ---------------
+
+select 
+    first_name , 
+    count(first_name) as name_count ,
+    hire_date
+from    
+    employees 
+where 
+    hire_date >= '1999-01-01'
+group by 
+    first_name 
+having count(first_name) < 200
+order by first_name ASC
+
+working with aggragartion function you should need to user HAVING 
 
 
-SELECT 
-    e.first_name, COUNT(e.first_name)
-FROM
-    employees e
-GROUP BY e.first_name;
-ORDER BY first_name ASC;
-
-
-
--- 
--- [+] add gruop by with aggragative function 
--- [+] add group by column to select statement 
+you cannot have both non-aggragated and aggregated condition in a same class 
