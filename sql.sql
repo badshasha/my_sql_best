b31a5b1fd875afb8f0f2fffec29061e859e6bce2
badshasha <shavendragoesoft@gmail.com>
2022-02-20 18:22:01 -0800

    select two tables

--- sql.sql
+++ sql.sql
@@ -1,10 +1,16 @@
--------------------------------------------
-------------- limit -----------------------
--------------------------------------------
+------------------
+--   using join query 
+
+
+select * from 
+    dept_manager dm
+    join
+    department d on dm.dept_no = d.dept_no
 
 select 
-    * 
-from 
-    salary 
-order by salary DESC 
-limit 10;
+    *
+ from 
+    table_one 
+    join
+    table_two 
+        on table_one.foregin_key = table_two.primary_key 
