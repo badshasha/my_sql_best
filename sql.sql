aba9dc1c0b5e68846bf6739c5961fcb6ea2a40ef
badshasha <shavendragoesoft@gmail.com>
2022-02-20 20:02:48 -0800

    nested select statetment

--- sql.sql
+++ sql.sql
@@ -1,14 +1,15 @@
----------------------------------------------------
-------- select more than two tables ---------------
+---------------------------------------------
+--------- nested selection ------------------
+---------------------------------------------
 
-SELECT 
-     t1.dept_no , t3.dept_name , count(t3.dept_name) as manager_number
-FROM
-    dept_manager t1
-        INNER JOIN
-            employees t2
-                ON t1.emp_no = t2.emp_no 
-                inner join
-                    departments t3
-                    on t1.dept_no = t3.dept_no
-                    group by t3.dept_name;
+select 
+    *
+from 
+    employees e 
+where 
+    e.emp_no in ( select 
+                    dm.emp_no
+                  from     
+                    dept_manager dm                   
+                    
+                );
